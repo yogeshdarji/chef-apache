46dbfeffdd5c18095334fb198363119392bcac86
yogeshdarji <yogesh_darji99@yahoo.com>
2020-03-03 18:10:14 +0000

    index.html includes node attributes for ipaddress and hostname

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,10 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello World</h1>'
+  content "<h1>Hello World</h1>
+  <h2>IPADDRESS: #{node['ipaddress'] } </h2>
+  <h2>HOSTNAME: #{node['hostname'] } </h2>
+"
 end
 
 service 'httpd' do
