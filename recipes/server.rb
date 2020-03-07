3e7c7351958eb6cea9c820eb8b912c39a238c25d
yogeshdarji <yogesh_darji99@yahoo.com>
2020-03-07 08:51:40 +0000

    refactored file resource to template for index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,10 +1,8 @@
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello World</h1>
-  <h2>IPADDRESS: #{node['ipaddress'] } </h2>
-  <h2>HOSTNAME: #{node['hostname'] } </h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
 end
 
 service 'httpd' do
