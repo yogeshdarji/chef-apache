b7478ac20b31cdab460432abe1aee2029132c00a
yogeshdarji <yogesh_darji99@yahoo.com>
2020-03-18 02:28:33 +0000

    Add index.html in template

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,8 +1,12 @@
 package 'httpd'
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  action :create
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#  action :create
+#end
+
+cookbook_file '/var/www/index.html' do
+  source 'index.html'
 end
 
 service 'httpd' do
